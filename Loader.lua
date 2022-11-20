7b30a989f7892dcd7249d1282aefbecd02bba3ba
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2022-11-20 17:09:27 -0600

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,7 +0,0 @@
-if game.PlaceId == 8003084678 then
-  return loadstring(game:HttpGet("https://pastebin.com/raw/FQQxqGwu"))()
-elseif game.PlaceId == 8712817601 then
-  return loadstring(game:HttpGet("https://pastebin.com/raw/zh2YLiXk"))
-else
-  return warn("Universal coming soon...")
-end
