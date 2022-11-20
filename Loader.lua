4ca11c46d0ab9b4fd3b60fc14a31647633ca5f17
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2022-11-20 01:26:07 -0600

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,8 +0,0 @@
--- OBBY HUB --
-local placeId = game.PlaceId
-local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zer0ids/Qwerty/main/obf1.lua"))()
-if placeId == 8003084678 then
-    ui:ChangeId(placeId)
-else
-    ui:ChangeId(0) -- universal coming soon...
-end
