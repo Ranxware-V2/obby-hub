7a62f43d76a2ffc1ac30d286c32a4db54bd11caa
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2022-11-20 11:00:24 -0600

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+local id = game.PlaceId
+if id == 8003088003084678 then
+  return loadstring(game:HttpGet("https://pastebin.com/raw/FQQxqGwu"))()
+else
+  return warn("Universal coming soon...")
+end
