10bcdee90f6c8f97509d16596d36b2d6583700dd
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2022-11-20 10:57:21 -0600

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,5 +0,0 @@
-if game.PlaceId == 8003084678 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Ranxware-V2/obby-hub/main/Games/625.lua?token=GHSAT0AAAAAABYTGDPBKLQD546XRYKYMT3OY32LQBQ"))()
-else
-  return false -- universal coming soon...
-end
