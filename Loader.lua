f1f726e8bcbc6fb418413be1e315cb9a1f3a1007
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2022-11-20 01:24:45 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@
 local placeId = game.PlaceId
 local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zer0ids/Qwerty/main/obf1.lua"))()
 if placeId == 8003084678 then
-    ui.PlaceId = placeId
+    ui:ChangeId(placeId)
 else
-    ui.PlaceId = nil -- universal coming soon...
+    ui:ChangeId(0) -- universal coming soon...
 end
