c098e24d81ed596993c51f9516fda471336217a3
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2022-11-20 10:51:34 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,5 +3,5 @@ wait(1)
 if game.PlaceId == 8003084678 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Ranxware-V2/obby-hub/main/Games/625.lua?token=GHSAT0AAAAAABYTGDPBCM7DMGFJIA3IDSMKY32LLUA"))()
 else
-  return -- universal coming soon...
+  return false -- universal coming soon...
 end
